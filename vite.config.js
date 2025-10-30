699308c03435e01a4d99f4cf00023d0818e56b56
ItsRovv <rovinjohnmantes097@gmail.com>
2025-10-30 22:04:03 +0800

    vercel

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  root: '.', // Ensure root is current directory
   build: {
     outDir: 'dist',
   },
